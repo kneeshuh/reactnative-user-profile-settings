d06967b7e54980ae99e1c8f54cab6298443f8f81
kneeshuh <ni.gettings@gmail.com>
2024-02-16 12:14:44 +0000

    add functionality for changing profile picture in editProfile page

--- screens/EditProfile.js
+++ screens/EditProfile.js
@@ -1,11 +1,69 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { useState } from 'react'
+import { MaterialIcons } from '@expo/vector-icons'
+import { colors } from '../constants/theme'
+import * as ImagePicker from 'expo-image-picker'
 
-const EditProfile = () => {
+const EditProfile = ({ navigation }) => {
+
+    const [selectedImage, setSelectedImage] = useState('https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=')
+
+    const handleImageSelection = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 4],
+            quality: 1
+        })
+        console.log(result)
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri)
+        }
+    }
+
     return (
-        <View>
-            <Text>Edit Profile</Text>
-        </View>
+        <SafeAreaView style={{
+            flex: 1,
+            backgroundColor: 'white',
+            paddingHorizontal: 22
+        }}>
+            <View style={{
+                marginHorizontal: 12,
+                flexDirection: 'row',
+                justifyContent: 'center'
+            }}>
+                <TouchableOpacity onPress={() => navigation.goBack()} style={{
+                    position: 'absolute',
+                    left: 0
+                }}>
+                    <MaterialIcons name='keyboard-arrow-left' size={24} color='black'/>
+                </TouchableOpacity>
+                <Text>Edit Profile</Text>
+            </View>
+            <ScrollView>
+                <View style={{
+                    alignItems: 'center',
+                    marginVertical: 22
+                }}>
+                    <TouchableOpacity onPress={handleImageSelection}>
+                        <Image source={{ uri: selectedImage }} style={{ height: 170, width: 170, borderRadius: 85, borderWidth: 2, borderColor: colors.primary }}/>
+                        <View style={{
+                            position: 'absolute',
+                            bottom: 0,
+                            right: 0,
+                            zIndex: 9999
+                        }}>
+                            <MaterialIcons 
+                                name='photo-camera'
+                                size={32}
+                                color={colors.primary}
+                            />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
--- screens/Settings.js
+++ screens/Settings.js
@@ -1,11 +1,92 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { MaterialIcons } from '@expo/vector-icons'
+import { ScrollView } from 'react-native'
 
-const SettingsScreen = () => {
+
+const SettingsScreen = ({ navigation }) => {
+
+    const navigateToEditProfile = () => {
+        navigation.navigate('EditProfile')
+    }
+
+    const navigateToSecurity = () => {
+        console.log('Security function')
+    }
+
+    const navigateToNotifications = () => {
+        console.log('Notifications function')
+    }
+
+    const navigateToPrivacy = () => {
+        console.log('Privacy function')
+    }
+
+    const accountItems = [
+        {icon: 'person-outline', text: 'Edit Profile', action: navigateToEditProfile},
+        // {icon: 'security', text: 'Security', action: navigateToSecurity},
+        // {icon: 'notifications-none', text: 'Notifications', action: navigateToNotifications},
+        // {icon: 'lock-outline', text: 'Privacy', action: navigateToPrivacy},
+    ];
+
+    const renderSettingsItem = ({ icon, text, action }) => {
+        <TouchableOpacity onPress={action} style={{ flexDirection: 'row', alignItems: 'center', paddingVertical: 8, paddingLeft: 12}}>
+            <MaterialIcons name={icon} size={24} color='black'/>
+            <Text style={{ marginLeft: 36, fontWeight: 600, fontSize: 16 }}>{text}</Text>
+        </TouchableOpacity>
+    }
+
     return (
-        <View>
-            <Text>Settings</Text>
-        </View>
+        <SafeAreaView
+            style={{
+                flex: 1,
+                backgroundColor: 'white'
+            }}
+        >
+            <View style={{
+                marginHorizontal: 12,
+                flexDirection: 'row',
+                justifyContent: 'center'
+            }}>
+                <TouchableOpacity
+                    onPress={() => navigation.goBack()}
+                    style={{
+                        position: 'absolute',
+                        left: 0
+                    }}
+                >
+                    <MaterialIcons 
+                        name='keyboard-arrow-left'
+                        size={24}
+                        color="black"
+                    />
+                </TouchableOpacity>
+                    <Text>Settings</Text>
+            </View>
+            <ScrollView style={{marginHorizontal: 12}}>
+
+            <View style={{
+                marginBottom: 12
+            }}>
+                <Text style={{
+                    marginVertical: 10
+                }}>Account</Text>
+                <View style={{
+                    borderRadius: 12,
+                }}> 
+                    <MaterialIcons 
+                        name='person-outline'
+                        size={24}
+                        color='black'
+                        onPress={navigateToEditProfile}
+                    ></MaterialIcons>
+                    <Text>Edit Profile</Text>
+
+                </View>
+            </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
